a93a246cae377a3816c607b3300a721aa27855f6
DigitalThought <matthew@digital-thought.org>
2021-04-14 17:09:36 +1000

    Added in ability to update digest with new content.

--- digital_thought_commons/digests/__init__.py
+++ digital_thought_commons/digests/__init__.py
@@ -1,41 +1,51 @@
 import hashlib
 
 
-def calc_string_digests(string: str, process_extended: bool = False):
-    return Digest(string.encode(encoding='utf-8'), process_extended=process_extended)
-
-
-def calc_bytes_digests(data: bytes, process_extended: bool = False):
-    return Digest(data, process_extended=process_extended)
-
-
 class Digest:
 
     def __init__(self, val: bytes, process_extended: bool = False) -> None:
         self._process_extended = process_extended
-        self._sha256 = hashlib.sha256(val).hexdigest()
-        self._sha1 = hashlib.sha1(val).hexdigest()
-        self._md5 = hashlib.md5(val).hexdigest()
+        self._sha256 = hashlib.sha256(val)
+        self._sha1 = hashlib.sha1(val)
+        self._md5 = hashlib.md5(val)
 
         self._sha512 = 'NOT_CALCULATED'
         if self._process_extended:
-            self._sha512 = hashlib.sha512(val).hexdigest()
+            self._sha512 = hashlib.sha512(val)
 
-    def sha256(self):
-        return self._sha256
+    def update_from_string(self, string: str) -> None:
+        self.update_from_bytes(string.encode(encoding='utf-8'))
 
-    def sha1(self):
-        return self._sha1
+    def update_from_bytes(self, data: bytes) -> None:
+        self._sha256.update(data)
+        self._sha1.update(data)
+        self._md5.update(data)
+        if self._process_extended:
+            self._sha512.update(data)
 
-    def md5(self):
-        return self._md5
+    def sha256(self) -> str:
+        return self._sha256.hexdigest()
 
-    def sha512(self):
-        return self._sha512
+    def sha1(self) -> str:
+        return self._sha1.hexdigest()
+
+    def md5(self) -> str:
+        return self._md5.hexdigest()
+
+    def sha512(self) -> str:
+        return self._sha512.hexdigest()
 
     def as_dict(self) -> dict:
-        json = {'md5': self._md5, 'sha1': self._sha1, 'sha256': self._sha256}
+        json = {'md5': self.md5(), 'sha1': self.sha1(), 'sha256': self.sha256()}
         if self._process_extended:
-            json['sha512'] = self._sha512
+            json['sha512'] = self.sha512()
 
         return json
+
+
+def calc_bytes_digests(data: bytes, process_extended: bool = False) -> Digest:
+    return Digest(data, process_extended=process_extended)
+
+
+def calc_string_digests(string: str, process_extended: bool = False) -> Digest:
+    return calc_bytes_digests(string.encode(encoding='utf-8'), process_extended=process_extended)
--- tests/digests_tests.py
+++ tests/digests_tests.py
@@ -27,3 +27,12 @@ class TestDigests(unittest.TestCase):
         self.assertEqual(json['sha256'], '717AC506950DA0CCB6404CDD5E7591F72018A20CBCA27C8A423E9C9E5626AC61'.lower())
         self.assertEqual(json['sha1'], 'E2F67C772368ACDEEE6A2242C535C6CC28D8E0ED'.lower())
         self.assertEqual(json['sha512'], 'B8EE69B29956B0B56E26D0A25C6A80713C858CF2902A12962AAD08D682345646B2D5F193BBE03997543A9285E5932F34BAF2C85C89459F25BA1CF43C4410793C'.lower())
+
+    def test_update_digests(self):
+        digest = digests.calc_string_digests("This is a test string", process_extended=True)
+        digest.update_from_string("and another")
+        self.assertEqual(digest.md5(), '9cc715fc7ee868e69464b85af305bf47')
+        self.assertEqual(digest.sha256(), 'c42d312cc3f1e09e56109791e81be10953055b06b8fa729e760a4a04892ec0f4')
+        self.assertEqual(digest.sha1(), '3cb90dea5074849a0da1c734c7738fe7db6523a6')
+        self.assertEqual(digest.sha512(), '4227b48563622f64b4e9c7afcef8564b829ca18691cf332f5b97b22ac8e0f1aca03c0370132932e1105b1cec5662e57de679fde56aa0a211c88904bb41a2409c')
+
