4ec110d5618a9dd7e6faf7a6d81a83524c79a020
DigitalThought <matthew@digital-thought.org>
2021-04-14 14:43:56 +1000

    Error in init reading of version file.

--- digital_thought_commons/__init__.py
+++ digital_thought_commons/__init__.py
@@ -1,6 +1,7 @@
 from . import *
+import pathlib
 
-with open("version", "r") as fh:
+with open("{}/version".format(str(pathlib.Path(__file__).parent.absolute())), "r") as fh:
     version_info = fh.read()
 
 __version__ = version_info
